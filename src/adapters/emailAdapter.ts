01e0c4cf9b2bc7a2ea4e970d4e469d085ca9c386
devliss <liseyna1@gmail.com>
2022-10-18 20:12:24 +0300

    v6

--- src/adapters/emailAdapter.ts
+++ src/adapters/emailAdapter.ts
@@ -5,22 +5,33 @@ export const emailAdapter = {
     async send(user:any,subject:string, message:string) {
         let transporter = nodemailer.createTransport({
             //service: "gmail",
-            host:"smtp.yandex.ru",
+            //host:"smtp.yandex.ru",
+            host:"smtp.mail.ru",
             port: 465,
             secure: true,
+            /*host:"imap.mail.ru",
+            port:993,
+            secure:true,*/
             auth: {
+                /*user:"devliss@inbox.ru",
+                pass:"7XskYYxBwkd9"*/
                 user: "devliss@yandex.ru",//process.env.SMTP_USER || "",
                 pass: "u*qY5tTJ*w53f" //process.env.SMTP_PASSWORD || ""
             }
         });
 
-        let result = await transporter.sendMail({
-            from:  "devliss@yandex.ru",
-            to: user.email,
-            subject: subject,
-            html: message
-        })
+        try {
+            let result = await transporter.sendMail({
+                from: "devliss@yandex.ru",
+                to: user.email,
+                subject: subject,
+                html: message
+            })
+            console.log(result)
+            return result
+        }catch(e){
+            console.log(e)
+        }
 
-        return result
     }
 }
